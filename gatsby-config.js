400df4358e73286d592ebe428c7ac3a89d192ca7
Jean Carlos Guimarães <guimaraes.jean95@gmail.com>
2021-03-01 08:39:02 -0300

    Cleaning starter stuff

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,8 +1,9 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
+    // TODO: Colocar os metadata do meu blog ao final
+    title: `My Blog`,
+    description: `A blog about Front-End development and other cool stuff.`,
+    author: `@eumesmo`,
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
