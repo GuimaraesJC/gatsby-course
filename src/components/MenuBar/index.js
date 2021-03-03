356f146ef9852cc6133329f3eaac06d6eb84cbf4
Jean Carlos Guimarães <guimaraes.jean95@gmail.com>
2021-03-03 14:33:41 -0300

    A lot of things

--- src/components/MenuBar/index.js
+++ src/components/MenuBar/index.js
@@ -4,7 +4,6 @@ import { Home } from '@styled-icons/boxicons-solid/Home'
 import { SearchAlt2 as Search } from '@styled-icons/boxicons-regular/SearchAlt2'
 import { UpArrowAlt as Arrow } from '@styled-icons/boxicons-regular/UpArrowAlt'
 import { Bulb as Light } from '@styled-icons/boxicons-regular/Bulb'
-import { Grid } from '@styled-icons/boxicons-solid/Grid'
 
 import * as S from "./styled"
 
@@ -25,9 +24,6 @@ const MenuBar = () => (
     <S.MenuBarGroup>
       <S.MenuBarItem title="Mudar o tema">
         <Light />
-      </S.MenuBarItem>
-      <S.MenuBarItem title="Mudar viualização">
-        <Grid />
       </S.MenuBarItem>
       <S.MenuBarItem title="Ir para o topo">
         <Arrow />
